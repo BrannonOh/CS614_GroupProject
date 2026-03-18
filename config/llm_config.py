7266681910f7a2e5ef535f0b6a4a68589a083af2
Brannon Oh <brannonoh@gmail.com>
2026-03-18 14:40:56 +0800

    file for all LLMs

---
+++ config/llm_config.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+
+from schemas.ted_blueprint import TEDBlueprint
+from schemas.structure_checking import StructureCheckOutput
+from schemas.judging_output import JudgingOutput
+
+def ted_llm():
+    llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+    ted_llm = llm.with_structured_output(TEDBlueprint)
+    return ted_llm
+
+def structure_llm():
+    llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+    structure_llm = llm.with_structured_output(StructureCheckOutput)
+    return structure_llm
+
+def ted_revision_llm():
+    llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+    ted_revision_llm = llm.with_structured_output(TEDBlueprint)
+    return ted_revision_llm
+
+def judging_llm():
+    llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+    judging_llm = llm.with_structured_output(JudgingOutput)
+    return judging_llm
